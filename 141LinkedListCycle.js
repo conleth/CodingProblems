4c6f255442d94c64a7d74a7b08b803c2d26b214f
conleth <conlethkennedy@gmail.com>
2024-06-18 10:16:28 -0700

    Create 141LinkedListCycle.js

---
+++ 141LinkedListCycle.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+// var hasCycle = function(head) {
+//     if (head == null) {
+//         return false;
+//     }
+//     let curr = head;
+//     const seenNodes = new Set();
+//     while (curr != null) {
+//         if (seenNodes.has(curr)) {
+//             return true;
+//         } else {
+//             seenNodes.add(curr);
+//         }
+//         curr = curr.next;
+//     }
+//     return false;
+// };
+
+var hasCycle = function(head) {
+    let fast = head;
+    let slow = head;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (fast === slow) {
+            return true;
+        }
+    }
+
+    return false;    
+};
